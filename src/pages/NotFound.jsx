edfdd72c4e883848c2f4e0547af4a4697716008d
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-02-05 19:48:14 -0500

    commit por las dudas antes de corregir todo el proyecto jeje

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/NotFound.scss'
+
+const NotFound = () => {
+    return (
+        <div className="not-found-container">
+          <div className="not-found-info">
+            <h1 className="not-found-title">Error 404.</h1>
+            <h2>This page is not found.</h2>
+            <button className="primary-button">Go Home</button>
+          </div>
+        </div>
+    );
+}
+
+export default NotFound;
