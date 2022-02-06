c7263008f293324587f4ffcc9762f7c378119a25
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-02-06 18:21:18 -0500

    se agregaron todas las imadenes y se crearon los alias para las rutas,FUNCIONA OK

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import '../styles/NotFound.scss'
 
 const NotFound = () => {
 	return (
 		<div>
-			<div className="not-found-container">
-			<div className="not-found-info">
-				<h1 className="not-found-title">Error 404.</h1>
-				<h2>This page is not found.</h2>
-				<button className="primary-button">Go Home</button>
-			</div>
-			</div>
+			No encontrado, error 404.
 		</div>
 	);
 }
