f1b624eccf57955392727212e09a48f9dab2cd87
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-02-07 20:50:58 -0500

    clase eliminando articulos del carrito OK

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -14,9 +14,19 @@ const useInitialState = () => {
 		});
 	};
 
+	const removeFromCart = (payload) => {
+		setState({
+			...state,
+			cart: state.cart.filter(items => items.id !== payload.id),
+		});
+	}
+
+
+
 	return {
 		state,
-		addToCart
+		addToCart,
+		removeFromCart
 	}
 }
 
